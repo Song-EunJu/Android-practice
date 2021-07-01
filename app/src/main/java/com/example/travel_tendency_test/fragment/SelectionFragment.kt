096bc65a20edc15d778fb3e4c512de5ca9ceef5f
Song-EunJu <jd06280@naver.com>
2021-07-02 00:05:09 +0900

    [FIN]test application

--- app/src/main/java/com/example/travel_tendency_test/fragment/SelectionFragment.kt
+++ app/src/main/java/com/example/travel_tendency_test/fragment/SelectionFragment.kt
@@ -5,30 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.travel_tendency_test.R
+import kotlinx.android.synthetic.main.fragment_selection.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SelectionFragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SelectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SelectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +23,42 @@ class SelectionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_selection, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btn_back.setOnClickListener(this)
+        option_1.setOnClickListener(this)
+        option_2.setOnClickListener(this)
+        option_3.setOnClickListener(this)
+        option_4.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) { // ? 를 쓰면 view 가 null이 아니면 id를 가져온다.
+            R.id.option_1 -> {
+                navigateWithIndex(1)
+            }
+            R.id.option_2 -> {
+                navigateWithIndex(2)
+            }
+            R.id.option_3 -> {
+                navigateWithIndex(3)
+            }
+            R.id.option_4 -> {
+                navigateWithIndex(4)
+            }
+
+            R.id.btn_back -> {
+                navController.popBackStack()
+                //fragment는 이동할 때 stack에 쌓임, a->b->c 순서대로 이동했으면 stack에 a가 밑바닥부터 쌓임
+                // 뒤로가기를 누르면 stack에 있는 거를 하나 pop해주면 됨
+            }
+        }
+    }
+
+    fun navigateWithIndex(index: Int) {  // fragment를 데이터로 보내기 위해서는 bundle 로 싸서 보내줌
+        val bundle = bundleOf("index" to index) // 위의 파라미터로 오는 index를 index라는 키에 넣어준다
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment, bundle)
+    }
+
 }
